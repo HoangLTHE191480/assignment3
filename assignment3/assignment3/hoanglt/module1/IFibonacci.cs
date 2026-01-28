eaa55dc8ef39ec88a20dcdc7da03c7dc037fa4e9
hoang <hgle249305@gmail.com>
2026-01-28 23:18:19 +0700

    Add inteface Fibonacci

---
+++ assignment3/assignment3/hoanglt/module1/IFibonacci.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3.hoanglt.module1
+{
+    internal interface IFibonacci
+    {
+        int Fun(int n, bool flag);
+
+    }
+}
