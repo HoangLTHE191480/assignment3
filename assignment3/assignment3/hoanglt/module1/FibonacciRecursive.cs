0a70f29e7bee10262ef5cb835355fa9f57cdc20b
hoang <hgle249305@gmail.com>
2026-01-28 23:27:01 +0700

    Fibonacci using recursion

---
+++ assignment3/assignment3/hoanglt/module1/FibonacciRecursive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3.hoanglt.module1
+{
+    public class FibonacciRecursive
+    {
+        public int Fun(int n, bool flag)
+        {
+            if (!flag)
+                return -1;
+
+            return Fib(n);
+        }
+
+        private int Fib(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
