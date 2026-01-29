a78a7726106c8d9fcbe1a5d594cc09f27e4dae7b
hungntm <hungminhml26@gmail.com>
2026-01-29 10:28:51 +0700

    Implement Fibonacci recursive (flag=true)

---
+++ assignment3/assignment3/hungntm/module1/FibonacciService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3.hungntm.module1
+{
+    public class FibonacciService : IFibonacci
+    {
+        public int funFibonacci(int n, bool flag)
+        {
+            if (!flag) return -1;
+            if (n <= 1) return n;
+            return funFibonacci(n - 1, true) + funFibonacci(n - 2, true);
+        }
+    }
+}
